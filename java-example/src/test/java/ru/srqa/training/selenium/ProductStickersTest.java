83ad49ae14743c79209eff7b08470a52e6b2b50a
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2019-04-19 16:37:28 +0600

    Add test to check each product has one sticker

---
+++ java-example/src/test/java/ru/srqa/training/selenium/ProductStickersTest.java
@@ -0,0 +1,35 @@
+package ru.srqa.training.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import util.PropertyLoader;
+
+import java.util.List;
+
+public class ProductStickersTest extends TestBase {
+
+    @Test
+    public void testProductStickers() {
+        driver.get(PropertyLoader.loadProperty("mainpage.url"));
+
+        Assert.assertTrue(checkEachProductHaveOneSticker());
+    }
+
+    public boolean checkEachProductHaveOneSticker() {
+        boolean stickerIsPresent = true;
+        List<WebElement> productList = driver.findElements(By.cssSelector("li.product.column.shadow.hover-light "));
+
+        for (WebElement product : productList) {
+
+            int amountOfStickersPerProduct = product.findElements(By.xpath(".//div[contains(@class,'sticker')]")).size();
+
+            if (amountOfStickersPerProduct != 1) {
+                stickerIsPresent = false;
+                break;
+            }
+        }
+        return stickerIsPresent;
+    }
+}
