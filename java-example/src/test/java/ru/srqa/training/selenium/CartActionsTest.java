e73ab6b1b14b8097989162802a264d823c83b87a
maria_belyaeva_qa <maria.belyaeva.qa@gmail.com>
2019-04-29 17:08:07 +0600

    add test to check adding and removing product from cart

---
+++ java-example/src/test/java/ru/srqa/training/selenium/CartActionsTest.java
@@ -0,0 +1,49 @@
+package ru.srqa.training.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import util.PropertyLoader;
+
+public class CartActionsTest extends TestBase {
+    public static final int AMOUNT_OF_PRODUCTS_TO_ADD = 3;
+    public static final int AMOUNT_OF_PRODUCTS_TO_REMOVE = 3;
+
+    @Test
+    public void testAddingProductToCart() {
+        driver.get(PropertyLoader.loadProperty("mainpage.url"));
+
+        addProductsToCart(AMOUNT_OF_PRODUCTS_TO_ADD);
+        Assert.assertEquals(AMOUNT_OF_PRODUCTS_TO_ADD, getAmountOfProductsInCart());
+        removeProductsFromCart(AMOUNT_OF_PRODUCTS_TO_REMOVE);
+        Assert.assertEquals(AMOUNT_OF_PRODUCTS_TO_ADD - AMOUNT_OF_PRODUCTS_TO_REMOVE, getAmountOfProductsInCart());
+    }
+
+    public void addProductsToCart(int amount) {
+        for (int i = 1; i < amount + 1; i++) {
+            driver.findElement(By.xpath("(//li[contains(@class,'product')])[" + i + "]")).click();
+            driver.findElement(By.cssSelector("button[name='add_cart_product']")).click();
+            WebDriverWait wait = new WebDriverWait(driver, 3);
+            wait.until(ExpectedConditions.textToBe(By.cssSelector("span.quantity"), String.valueOf(i)));
+            driver.navigate().back();
+        }
+    }
+
+    private void removeProductsFromCart(int amount) {
+        driver.findElement(By.xpath("//a[contains(text(),'Checkout')]")).click();
+
+        for (int i = 1; i < amount + 1; i++) {
+            driver.findElement(By.xpath("(//button[@name='remove_cart_item'])[1]")).click();
+
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("tr td.item"), (amount - i)));
+        }
+        driver.findElement(By.xpath("//a[contains(text(),'Back')]")).click();
+    }
+    
+    public int getAmountOfProductsInCart() {
+        return  Integer.valueOf(driver.findElement(By.cssSelector("span.quantity")).getAttribute("innerText"));
+    }
+}
